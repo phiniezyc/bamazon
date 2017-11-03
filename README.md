d9ffdbee3edebae155be2a8b98ea5c4dae5715cd
Chance Phiniezy <phiniezyc@gmail.com>
2017-11-03 16:39:09 -0400

    inserted data into products tabble

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # bamazon
 Amazon-like storefront command line app that uses MySQL to take orders and deplete stock from the store's inventory.
+
--- bamazon.sql
+++ bamazon.sql
@@ -3,10 +3,42 @@ CREATE DATABASE bamazon_DB;
 USE bamazon_DB;
 
 CREATE TABLE products(
-  item_id INT NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(100) NOT NULL,
   dapartment_name VARCHAR(100) NOT NULL,
   price DECIMAL(8, 2) NOT NULL,
   stock_quantity INT NOT NULL,
   PRIMARY KEY (id)
 );
+
+
+
+INSERT INTO products (product_name, dapartment_name, price, stock_quantity)
+VALUES ("football", "sports" , 39.95, 25);
+
+INSERT INTO products (product_name, dapartment_name, price, stock_quantity)
+VALUES ("cleats", "sports" , 119.95, 55);
+
+INSERT INTO products (product_name, dapartment_name, price, stock_quantity)
+VALUES ("canoes", "outdoors" , 499.95, 9);
+
+INSERT INTO products (product_name, dapartment_name, price, stock_quantity)
+VALUES ("xbox", "gaming" , 499.95, 200);
+
+INSERT INTO products (product_name, dapartment_name, price, stock_quantity)
+VALUES ("guitar", "music" , 999.95, 15);
+
+INSERT INTO products (product_name, dapartment_name, price, stock_quantity)
+VALUES ("blender", "cooking" , 49.95, 6);
+
+INSERT INTO products (product_name, dapartment_name, price, stock_quantity)
+VALUES ("backpack", "school" , 35.99, 30);
+
+INSERT INTO products (product_name, dapartment_name, price, stock_quantity)
+VALUES ("computer", "computing" , 1400.00, 42);
+
+INSERT INTO products (product_name, dapartment_name, price, stock_quantity)
+VALUES ("tent", "outdoors" , 199.95, 17);
+
+INSERT INTO products (product_name, dapartment_name, price, stock_quantity)
+VALUES ("leash", "pets" , 24.95, 14);
